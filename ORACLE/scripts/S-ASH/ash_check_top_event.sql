7b76a7d5baeecbfd27f1083b8792358330917187
Scofieldd <kouss.hd@gmail.com>
2018-09-13 17:15:43 -0400

    time waited count(*)

--- ORACLE/scripts/S-ASH/ash_check_top_event.sql
+++ ORACLE/scripts/S-ASH/ash_check_top_event.sql
@@ -21,7 +21,7 @@ col sql_id for a13
 col USERNAME for a10
 select * from (
       -- sub1: one row per sampled ASH observation second
-      select min(sample_time) begin_time,max(sample_time) end_time,WAIT_CLASS,event, nvl(sql_id,'-------------') sql_id, count(*) count,sum(TIME_WAITED) time_waited, trunc(avg(time_waited),2)  avg_time_waited
+      select min(sample_time) begin_time,max(sample_time) end_time,WAIT_CLASS,event, nvl(sql_id,'-------------') sql_id,count(*) time_waited, trunc(avg(time_waited),2)  avg_time_waited
   From v$active_session_history   --- 1 second samples
   --  FROM dba_hist_active_sess_history    -- 10 seconds samples  
      Where  sample_time between trunc(sysdate -&&num_days)+&&begin_H/24+&&begin_m/1440 and trunc(sysdate -&&num_days)+&&end_H/24+&&end_m/1440
@@ -38,7 +38,7 @@ prompt *** per session_ID SQL_ID
 col username for a15
 select * from (
       -- sub1: one row per sampled ASH observation second
-      select min(sample_time) begin_time,max(sample_time) end_time,session_id,dba_users.username,WAIT_CLASS,sql_id,sum(TIME_WAITED) time_waited,trunc(avg(time_waited),2)  avg_time_waited,count(*) count,event
+      select min(sample_time) begin_time,max(sample_time) end_time,session_id,dba_users.username,WAIT_CLASS,sql_id,count(*) time_waited,trunc(avg(time_waited),2)  avg_time_waited,event
   From v$active_session_history ,dba_users  --- 1 second samples
      -- FROM dba_hist_active_sess_history    -- 10 seconds samples
      Where  sample_time between trunc(sysdate -&&num_days)+&&begin_H/24+&&begin_m/1440 and trunc(sysdate -&&num_days)+&&end_H/24+&&end_m/1440
@@ -53,7 +53,7 @@ select * from (
 
 /*
 
-            select min(sample_time) begin_time,max(sample_time) end_time,session_id,dba_users.username,WAIT_CLASS,sql_id,sum(TIME_WAITED) time_waited,trunc(avg(time_waited),2)  avg_time_waited,count(*) count,event
+            select min(sample_time) begin_time,max(sample_time) end_time,session_id,dba_users.username,WAIT_CLASS,sql_id,count(*) time_waited,trunc(avg(time_waited),2)  avg_time_waited,event
   From v$active_session_history ,dba_users  --- 1 second samples
      -- FROM dba_hist_active_sess_history    -- 10 seconds samples
      Where  sample_time between trunc(sysdate -&&num_days)+&&begin_H/24+&&begin_m/1440 and trunc(sysdate -&&num_days)+&&end_H/24+&&end_m/1440
